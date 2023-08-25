729b6e2999f8534b34b9a234a84ac7bcb35aa9e2
RitchielRez <ritchielrez@gmail.com>
2023-08-25 19:55:22 +0600

    feat(sizeof): updtaes

--- sizeof.cpp
+++ sizeof.cpp
@@ -3,9 +3,11 @@
 int main() {
   int arr[] = {1, 2, 3, 4, 5};
   std::size_t size_arr = sizeof(arr);
+  std::size_t size_arr_plus_1 = sizeof(arr) + 1;
   int *ptr = arr;
 
   std::cout << "Size of the arr: " << size_arr << "\n";
+  std::cout << "sizeof(arr) + 1: " << size_arr_plus_1 << "\n";
   std::cout << ptr << "\n";
   std::cout << ptr + 1 << "\n";
 }
